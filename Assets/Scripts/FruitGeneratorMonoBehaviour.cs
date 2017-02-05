bdd901fbaee83e3ac87eb09d34967c45d29004ee
SumanConstantin <SumanConstantin@gmail.com>
2017-02-05 20:28:28 +0200

    Basic functionality prototype
    Basic functionality prototype:
    - 2 swords
    - cuttable objects
    - environment setup

---
+++ Assets/Scripts/FruitGeneratorMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitGeneratorMonoBehaviour : MonoBehaviour {
+
+    public GameObject[] fruitPrefabs;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GenerateFruit());
+	}
+
+    private IEnumerator GenerateFruit()
+    {
+        while(true)
+        {
+            GameObject go = Instantiate(fruitPrefabs[Random.Range(0, fruitPrefabs.Length)]);
+            Rigidbody rigidBody = go.GetComponent<Rigidbody>();
+
+            rigidBody.velocity = new Vector3(0f, 5f, .5f);
+            rigidBody.angularVelocity = new Vector3(Random.Range(-4,4), 0f, Random.Range(-4,4));
+            rigidBody.useGravity = true;
+
+            Vector3 position = transform.position;
+            position.x += Random.Range(-1f, 1f);
+            rigidBody.transform.position = position;
+
+            yield return new WaitForSeconds(1);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/KatanaMonoBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class KatanaMonoBehaviour : MonoBehaviour
+{
+    public SteamVR_TrackedObject trackedObj;
+    public Material capMaterial;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+        {
+            pieces[1].AddComponent<Rigidbody>();
+            MeshCollider meshColl = pieces[1].AddComponent<MeshCollider>();
+            meshColl.convex = true;
+        }
+
+        StartCoroutine(LongVibration(.5f, .5f));
+    }
+
+    IEnumerator LongVibration(float length, float strength)
+    {
+        for(float i = 0; i < length; i+= Time.deltaTime)
+        {
+            SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+        }
+        yield return null;
+    }
+}
